dcd4dc5f5c4dd3713b678541633ad1f3b32bd0c8
Frankie Mercado <oeluer@gmail.com>
2019-07-28 23:35:52 -0700

    Fix copy/paste error in custom-video-speed. Update script to be more programmatic.

--- script.js
+++ script.js
@@ -1,57 +1,30 @@
 const list = document.createElement('ol');
 document.body.appendChild(list);
 
-const addScript = (text, code) => {
+const readJS = async file => {
+  const response = await fetch(file);
+  const text = await response.text();
+  return text;
+}
+
+const toURI = async code => {
+  code = code.replace(/\n/g, '').replace(/\s+/g, ' ').replace(/ ?([{};,=?:()]) ?/g, '$1');
+  code = `javascript:(()=>{${code}})()`;
+  return encodeURI(code);
+}
+
+const addScript = (text, URI) => {
   const newItem = document.createElement('li');
   list.appendChild(newItem);
 
   const link = document.createElement('a');
-  link.href = encodeURI(code);
+  link.href = URI;
   link.textContent = text;
   newItem.appendChild(link);
 };
 
-addScript("Custom Video Speed", `javascript: (() => {
-        if (document.getElementsByTagName("video").length) {
-          let video = document.getElementsByTagName("video")[0];
-          let speed = video.playbackRate;
-          let input = Math.min(Math.max(parseFloat(prompt("How fast?\\n0x⟷16.0x", speed)), 0), 16);
-          video.playbackRate = isNaN(input) ? speed : input;
-        } else {
-          console.log("Can't find a video.");
-        }
-      })();`);
-
-addScript("Skip Ads, Toggle 1x/16x", `javascript: (() => {
-        if (document.getElementsByTagName("video").length) {
-          let video = document.getElementsByTagName("video")[0];
-          let speed = video.playbackRate;
-          video.playbackRate = speed == 16 ? 1 : 16;
-        } else {
-          console.log("Can't find a video.");
-        }
-      })();`);
-
-addScript("Power Hour", `javascript: (() => {
-  if (document.getElementsByClassName('ytp-next-button ytp-button').length) {
-    const emojis = n => {
-      const doubles = n => Math.floor(n / 2);
-      const singles = n => n % 2;
-      const faces = ['😋', '😛', '😜', '🤪', '😵', '🤯', '😎'];
-      return '🍻'.repeat(doubles(n)) + '🍺'.repeat(singles(n)) + faces[Math.floor(n / 10)] + '🍺'.repeat(singles(60 - n)) + '🍻'.repeat(doubles(60 - n));
-    };
-
-    let shots = 0;
-    console.log('Starting!');
-    const timer = setInterval(() => {
-      document.getElementsByClassName('ytp-next-button ytp-button')[0].click();
-      console.log(\`\${++shots} shot\${shots > 1 ? 's' : ''} down \${60 - shots} to go! \${emojis(shots)}\`);
-      if (shots === 60) {
-        clearInterval(timer);
-        alert('YOU POWERED THROUGH THE HOUR!');
-      }
-    }, 60 * 1000);
-  } else {
-    console.log("Can't find a video.");
-  }
-})();`);
+[
+  ["Custom Video Speed", "snippets/custom-video-speed.js"],
+  ["Skip Ads, Toggle 1x/16x", "snippets/skip-ads.js"],
+  ["Power Hour", "snippets/power-hour.js"],
+].forEach(async snippet => addScript(snippet[0], await toURI(await readJS(snippet[1]))));
--- snippets/custom-video-speed.js
+++ snippets/custom-video-speed.js
@@ -1,7 +1,8 @@
 if (document.getElementsByTagName("video").length) {
   let video = document.getElementsByTagName("video")[0];
   let speed = video.playbackRate;
-  video.playbackRate = speed == 16 ? 1 : 16;
+  let input = Math.min(Math.max(parseFloat(prompt("How fast?\n0x⟷16.0x", speed)), 0), 16);
+  video.playbackRate = isNaN(input) ? speed : input;
 } else {
   console.log("Can't find a video.");
 }
