38181eaab2cd4d125ea00fdb632b68f2965f84e3
Frankie Mercado <oeluer@gmail.com>
2019-10-04 15:05:47 -0700

    Update video speeds to find all videos

--- snippets/custom-video-speed.js
+++ snippets/custom-video-speed.js
@@ -1,8 +1,8 @@
 if (document.getElementsByTagName("video").length) {
-  let video = document.getElementsByTagName("video")[0];
-  let speed = video.playbackRate;
+  let videos = [...document.getElementsByTagName("video")];
+  let speed = videos[0].playbackRate;
   let input = Math.min(Math.max(parseFloat(prompt("How fast?\n0x⟷16.0x", speed)), 0), 16);
-  video.playbackRate = isNaN(input) ? speed : input;
+  videos.forEach(v => v.playbackRate = isNaN(input) ? speed : input);
 } else {
   console.log("Can't find a video.");
 }
--- snippets/skip-ads.js
+++ snippets/skip-ads.js
@@ -1,7 +1,7 @@
 if (document.getElementsByTagName("video").length) {
-  let video = document.getElementsByTagName("video")[0];
-  let speed = video.playbackRate;
-  video.playbackRate = speed == 16 ? 1 : 16;
+  let videos = [...document.getElementsByTagName("video")];
+  let speed = videos[0].playbackRate;
+  videos.forEach(v => v.playbackRate = speed == 16 ? 1 : 16);
 } else {
   console.log("Can't find a video.");
 }
