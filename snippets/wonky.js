7603c5f03c9e490fc09939971a88e0f11551ec00
Frankie Mercado <oeluer@gmail.com>
2020-06-07 17:16:21 -0700

    center random

--- snippets/wonky.js
+++ snippets/wonky.js
@@ -1,1 +1,1 @@
-[...document.querySelectorAll('*')].forEach(i=>i.style.transform=`rotate(${Math.random()}deg)`)
+[...document.querySelectorAll('*')].forEach(i=>i.style.transform=`rotate(${Math.random()-.5}deg)`)
