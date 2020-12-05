fd84f1e10e8b435bb5558ed293a4641e607b5431
Frankie Mercado <oeluer@gmail.com>
2020-12-05 02:27:26 -0800

    change regex to split

--- snippets/tiktok-download.js
+++ snippets/tiktok-download.js
@@ -1,1 +1,1 @@
-window.open(`https://ttdownloader.com/dl.php?v=${btoa(`a:4:{s:9:"watermark";b:0;s:7:"videoId";s:19:"${window.location.pathname.match(/\d+/)[0]}";s:3:"uid";s:32:"91f1130c08d98809966817231b2fddea";s:4:"time";i:${~~(Date.now() / 1000)};}`)}`);
+window.open(`https://ttdownloader.com/dl.php?v=${btoa(`a:4:{s:9:"watermark";b:0;s:7:"videoId";s:19:"${window.location.pathname.split('/').pop()}";s:3:"uid";s:32:"91f1130c08d98809966817231b2fddea";s:4:"time";i:${~~(Date.now() / 1000)};}`)}`);
