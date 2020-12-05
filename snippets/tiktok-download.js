955d59240ee1b097233363248356b693b59f6301
Frankie Mercado <oeluer@gmail.com>
2020-12-05 02:12:59 -0800

    put tiktok in single block

--- snippets/tiktok-download.js
+++ snippets/tiktok-download.js
@@ -1,5 +1,7 @@
-let id = window.location.pathname.match(/\d+/)[0];
-let date =  ~~(Date.now()/1000);
-let code = `a:4:{s:9:"watermark";b:0;s:7:"videoId";s:19:"${id}";s:3:"uid";s:32:"91f1130c08d98809966817231b2fddea";s:4:"time";i:${date};}`
-let url = `https://ttdownloader.com/dl.php?v=~${btoa(code)}`;
-window.open(url);
+{
+  let id = window.location.pathname.match(/\d+/)[0];
+  let date = ~~(Date.now() / 1000);
+  let code = `a:4:{s:9:"watermark";b:0;s:7:"videoId";s:19:"${id}";s:3:"uid";s:32:"91f1130c08d98809966817231b2fddea";s:4:"time";i:${date};}`
+  let url = `https://ttdownloader.com/dl.php?v=~${btoa(code)}`;
+  window.open(url);
+}
