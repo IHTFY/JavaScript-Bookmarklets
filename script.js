21aa86ce83dae81fe755aaa57a99fc826b2e97c6
Frankie Mercado <oeluer@gmail.com>
2020-12-05 02:16:52 -0800

    fix tiktok snippet path

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const arr = [
   ["Unix time of a tweet", "snippets/tweet-time.js"],
   ["Tongue Twister Solver", "snippets/tongue-twister.js"],
   ["Wonky Page", "snippets/wonky.js"],
-  ["TikTok Download", "tiktok-download.js"]
+  ["TikTok Download", "snippets/tiktok-download.js"]
 ];
 
 const codify = async snippet => [snippet[0], toURI(await readJS(snippet[1]))];
