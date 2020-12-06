8068c49adacd0bf86c2369d869847830f3990ea3
Frankie Mercado <oeluer@gmail.com>
2020-12-06 01:09:18 -0800

    xhr to fetch

--- snippets/tiktok-download.js
+++ snippets/tiktok-download.js
@@ -1,17 +1,12 @@
-let xhr = new XMLHttpRequest();
 let link = document.getElementsByTagName('video')[0].src;
 let vid = window.location.pathname.split('/').pop();
-xhr.open('GET', link, true);
-xhr.responseType = 'blob';
-xhr.onload = function () {
-  let urlCreator = window.URL || window.webkitURL;
-  let videoUrl = urlCreator.createObjectURL(this.response);
-  let tag = document.createElement('a');
-  tag.href = videoUrl;
-  tag.download = `${vid}.mp4`;
-  tag.click();
-};
-xhr.onerror = err => {
-  alert('Download Failed');
-};
-xhr.send();
+
+fetch(new Request(link))
+  .then(response => response.blob())
+  .then(function (b) {
+    let objectURL = URL.createObjectURL(b);
+    let tag = document.createElement('a');
+    tag.href = objectURL;
+    tag.download = `${vid}.mp4`;
+    tag.click();
+  })
