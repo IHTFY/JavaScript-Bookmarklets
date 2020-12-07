415ffd6ef9e39f659e7dbda55db83fb99fe408a7
Frankie Mercado <oeluer@gmail.com>
2020-12-07 13:35:42 -0800

    Add descriptions

--- script.js
+++ script.js
@@ -12,29 +12,69 @@ const toURI = code => {
   return encodeURI(code);
 };
 
-const addScript = (text, URI) => {
+const addScript = (label, URI, desc) => {
   const newItem = document.createElement('li');
   list.appendChild(newItem);
 
   const link = document.createElement('a');
   link.href = URI;
-  link.textContent = text;
+  link.textContent = label;
   newItem.appendChild(link);
+
+  const description = document.createElement('span');
+  description.textContent = desc;
+  newItem.appendChild(description);
 };
 
 const arr = [
-  ["Custom Video Speed", "snippets/custom-video-speed.js"],
-  ["Skip Ads, Toggle 1x/16x", "snippets/skip-ads.js"],
-  ["Power Hour", "snippets/power-hour.js"],
-  ["Soundcloud RSS", "snippets/soundcloud-rss.js"],
-  ["Unix time of a tweet", "snippets/tweet-time.js"],
-  ["Tongue Twister Solver", "snippets/tongue-twister.js"],
-  ["Wonky Page", "snippets/wonky.js"],
-  ["TikTok Video Download", "snippets/tiktok-download.js"],
-  ["TikTok Channel Download", "snippets/tiktok-all.js"]
+  {
+    title: "Custom Video Speed",
+    path: "snippets/custom-video-speed.js",
+    description: "Set the playback speed of all videos on the page (0x to 16x)",
+  },
+  {
+    title: "Skip Ads, Toggle 1x/16x",
+    path: "snippets/skip-ads.js",
+    description: "Toggle the playback speed of all videos between 0x and 16x",
+  },
+  {
+    title: "Power Hour",
+    path: "snippets/power-hour.js",
+    description: "Automatically go to the next youtube video every 60 seconds for 1 hour",
+  },
+  {
+    title: "SoundCloud RSS",
+    path: "snippets/soundcloud-rss.js",
+    description: "Get the RSS feed for a SoundCloud Channel",
+  },
+  {
+    title: "Unix time of a tweet",
+    path: "snippets/tweet-time.js",
+    description: "Copy the unix timecode for when a tweet was posted",
+  },
+  {
+    title: "Tongue Twister Solver",
+    path: "snippets/tongue-twister.js",
+    description: "Text to Speech with repetition option",
+  },
+  {
+    title: "Wonky Page",
+    path: "snippets/wonky.js",
+    description: "Slightly rotate each element on the webpage randomly",
+  },
+  {
+    title: "TikTok Video Download",
+    path: "snippets/tiktok-download.js",
+    description: "Download an individual TikTok video",
+  },
+  {
+    title: "TikTok Channel Download",
+    path: "snippets/tiktok-all.js",
+    description: "Download all the videos on a TikTok channel",
+  }
 ];
 
-const codify = async snippet => [snippet[0], toURI(await readJS(snippet[1]))];
+const extractJS = async snippet => [snippet.title, toURI(await readJS(snippet.path)), snippet.description];
 
-Promise.all(arr.map(codify))
-  .then(codifiedPairs => codifiedPairs.forEach(pair => addScript(...pair)));
+Promise.all(arr.map(extractJS))
+  .then(a => a.forEach(pair => addScript(...pair)));
