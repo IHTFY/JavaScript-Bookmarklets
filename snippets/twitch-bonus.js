4c214224f755bf4db3fc0767a603731e49b74989
Frankie Mercado <oeluer@gmail.com>
2021-04-17 23:50:52 -0700

    use optional chaining

--- snippets/twitch-bonus.js
+++ snippets/twitch-bonus.js
@@ -1,1 +1,1 @@
-javascript:setInterval(()=>document.querySelector('.claimable-bonus__icon').click(),1000);
+javascript:setInterval(()=>document.querySelector('.claimable-bonus__icon')?.click(),1000);
