52186de247dcacc684e3f18c70c97ce79e0ef8ca
Frankie Mercado <oeluer@gmail.com>
2021-10-30 02:18:42 -0700

    update tiktok channel downloader

--- snippets/tiktok-all.js
+++ snippets/tiktok-all.js
@@ -1,50 +1,44 @@
 let numThumbs = 0;
-let scroller;
 
 function dl() {
   let vid = window.location.pathname.split('/').pop();
 
-  fetch(new Request(document.getElementsByTagName('video')[0].src))
-    .then(response => response.blob())
-    .then(function (b) {
-      let objectURL = URL.createObjectURL(b);
-      let tag = document.createElement('a');
-      tag.href = objectURL;
-      tag.download = `${vid}.mp4`;
-      tag.click();
-    })
+  fetch(new Request(document.getElementsByTagName('video')[0].src)).then(response => response.blob()).then(function (b) {
+    let objectURL = URL.createObjectURL(b);
+    let tag = document.createElement('a');
+    tag.href = objectURL;
+    tag.download = `${vid}.mp4`;
+    tag.click();
+  })
 }
 
-function scrollAndLoad() {
-  document.getElementsByClassName('share-layout-content')[0].scrollIntoView(false);
-  if (document.getElementsByClassName('video-feed-item').length > numThumbs) {
-    numThumbs = document.getElementsByClassName('video-feed-item').length;
+function scrollDown() {
+  document.querySelector(`[data-e2e='user-post-item-list']`).scrollIntoView(false);
+
+  if (document.querySelectorAll(`[data-e2e='user-post-item']`).length > numThumbs) {
+    numThumbs = document.querySelectorAll(`[data-e2e='user-post-item']`).length;
+    setTimeout(scrollDown, 1000);
   } else {
-    clearInterval(scroller);
     console.log(numThumbs + ' videos found');
-    videoList = [...document.getElementsByClassName('video-feed-item')];
+    videoList = [...document.querySelectorAll(`[data-e2e='user-post-item']`)];
 
-    alert('Click on the Final Video Quickly');
+    videoList.pop().querySelector('a').click();
 
-    setTimeout(() => {
-
-      function downloadAll() {
-        let left = document.querySelector('.arrow-left');
-        dl();
-
-        setTimeout(() => {
-          if (!!left) {
-            left.click();
-            setTimeout(downloadAll, 1500);
-          }
-        }, 1500);
-      }
-
-      downloadAll();
-    }, 3000
-    );
-
+    setTimeout(dlNext, 1500);
   }
 }
 
-scroller = setInterval(scrollAndLoad, 2000);
+scrollDown();
+
+function dlNext() {
+  dl();
+  const left = document.querySelector(`[data-e2e='arrow-left']:not([disabled])`);
+  if (left) {
+    left.click();
+    setTimeout(() => {
+      dlNext();
+    }
+      , 1500);
+  }
+}
+
