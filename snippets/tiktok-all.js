ad6b9237e9568ba22ff46a1af6290bc7697fdd37
Frankie Mercado <oeluer@gmail.com>
2021-10-30 02:44:10 -0700

    small refactor

--- snippets/tiktok-all.js
+++ snippets/tiktok-all.js
@@ -1,44 +1,37 @@
 let numThumbs = 0;
 
-function dl() {
+const dl = link => {
   let vid = window.location.pathname.split('/').pop();
 
-  fetch(new Request(document.getElementsByTagName('video')[0].src)).then(response => response.blob()).then(function (b) {
-    let objectURL = URL.createObjectURL(b);
-    let tag = document.createElement('a');
+  fetch(new Request(link)).then(response => response.blob()).then(b => {
+    const objectURL = URL.createObjectURL(b);
+    const tag = document.createElement('a');
     tag.href = objectURL;
     tag.download = `${vid}.mp4`;
     tag.click();
   })
 }
 
-function scrollDown() {
-  document.querySelector(`[data-e2e='user-post-item-list']`).scrollIntoView(false);
+const dlNext = () => {
+  dl(document.querySelector('video').src);
+  const left = document.querySelector(`[data-e2e='arrow-left']:not([disabled])`);
+  if (left) {
+    left.click();
+    setTimeout(dlNext, 1500);
+  }
+}
 
-  if (document.querySelectorAll(`[data-e2e='user-post-item']`).length > numThumbs) {
-    numThumbs = document.querySelectorAll(`[data-e2e='user-post-item']`).length;
-    setTimeout(scrollDown, 1000);
+const scrollDown = () => {
+  document.querySelector(`[data-e2e='user-post-item-list']`).scrollIntoView(false);
+  const thumbs = document.querySelectorAll(`[data-e2e='user-post-item']`);
+  if (thumbs.length > numThumbs) {
+    numThumbs = thumbs.length;
+    setTimeout(scrollDown, 750);
   } else {
     console.log(numThumbs + ' videos found');
-    videoList = [...document.querySelectorAll(`[data-e2e='user-post-item']`)];
-
-    videoList.pop().querySelector('a').click();
-
+    [...thumbs].pop().querySelector('a').click();
     setTimeout(dlNext, 1500);
   }
 }
 
 scrollDown();
-
-function dlNext() {
-  dl();
-  const left = document.querySelector(`[data-e2e='arrow-left']:not([disabled])`);
-  if (left) {
-    left.click();
-    setTimeout(() => {
-      dlNext();
-    }
-      , 1500);
-  }
-}
-
