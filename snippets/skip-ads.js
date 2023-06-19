7641d70f5156cda33a215d18dba68c8475517421
Frankie <oeluer@gmail.com>
2023-06-18 22:05:23 -0700

    use 1 as datum

--- snippets/skip-ads.js
+++ snippets/skip-ads.js
@@ -1,7 +1,7 @@
 if (document.getElementsByTagName("video").length) {
   const videos = [...document.getElementsByTagName("video")];
   const currentSpeed = videos[0].playbackRate;
-  videos.forEach(v => v.playbackRate = currentSpeed == 16 ? 1 : 16);
+  videos.forEach(v => v.playbackRate = currentSpeed == 1 ? 16 : 1);
 } else {
   console.log("Can't find a video.");
 }
